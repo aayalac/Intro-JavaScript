5df896582442f442a6af1fef5662bfdf94c70620
aayalac <>
2022-10-01 06:05:27 -0500

    Modificacion del index

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const max = (...arr) => arr.reduce(function(preValue,curValue){
-    if(preValue > curValue){
-      return preValue
-    }else{
-      return curValue
-    }
-})
-
-// código de prueba
-console.log(max([1, 2, 3, 4])) // 4
-console.log(max([63, 85, 39, 24, 3])) // 85
+console.log()
