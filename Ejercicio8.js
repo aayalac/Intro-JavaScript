a957c17f80cbe0a3ed2968b3571440393d7396f7
aayalac <aayalac129@gmail.com>
2022-10-01 10:31:21 -0500

    pruebas de codigo

--- Ejercicio8.js
+++ Ejercicio8.js
@@ -1,5 +1,5 @@
 num = parseFloat(prompt('Ingrese un numero multipo de 5: '))
 if(num % 5)
-  console.log(si, el numero ' + ' es multiplo de 5)
+  console.log("si, el numero " + num + "es multiplo de 5")
 else
-  console.log(No, el numero ' + ' no es multiplo de 5)
+  console.log("No, el numero " + num + "no es multiplo de 5")
